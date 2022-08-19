c6bd75a903d0a01f5593d0e423dd96a3966907fb
Woody White <woody.white276@gmail.com>
2022-08-19 09:16:44 -0700

    initial commit

---
+++ sdlc.js
@@ -0,0 +1,34 @@
+// In this section you will answer some work through problems related to the Software Development Lifecycle
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Reorganize the following steps of the Software Development Lifecycle to be in their correct order.
+*/
+
+// Implementation
+// Design
+// Maintain
+// Analysis of Requirements
+// Testing & Integration
+// Planning
+
+//1. Planning
+//2. Analysis of Requirements
+//3. Design
+//4. Implementation
+//5. Testing & Integration
+//6. Maintain
+
+
+
+//////////////////PROBLEM 2////////////////////
+/*
+Create a variable for each of the above steps of the Software Development Lifecycle and set the values of each variable to a definition of the step.
+The first one is done for you. 
+*/
+const planning = "This is the first step, in which the end goal of the software is determined and defined. Developers typically will see what systems are already in place, and determine what features will need to be added." 
+const analysisOfRequirements = "This is the second step, determining what tools are needed in order to execute the goal, such as coding languages and tech stacks, software methodology, hardware requirements, and roles to be filled."
+const design = "This is the third step, using the goals and tools to establish parameters, create (typically using pseudocode) an ideal environment and setup in order for the application to function as desired/instructed with the customer."
+const implementation = "This is the fourth step, which is translating the pseudocode into the properly functioning syntax of the chosen coding language that the application will be using. i.e. into Swift to function for Apple devices."
+const testingAndIntegration = "This is the fifth step, used to check for bugs, syntax errors, contradictions in the code, etc before releasing the application. Then integrating the application to suitably work in the chosen environment."
+const maintain = "This is the sixth step, once the application is out and functioning, ensuring continued proper operation in an ever changing environment as technology evolves, taking into account feedback to remedy errors."
